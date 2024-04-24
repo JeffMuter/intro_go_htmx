e746e04962950227fc4888aa5d23ce994f9d2404
JeffMuter <muterjeffery@gmail.com>
2024-04-24 00:21:58 -0400

    fixed the email. posts correctly. dummy user data added.

--- cmd/main.go
+++ cmd/main.go
@@ -20,6 +20,33 @@ func newTemplate() *Templates {
 	return &Templates{templates: template.Must(template.ParseGlob("views/*.html"))}
 }
 
+type Contact struct {
+	Name  string
+	Email string
+}
+
+func newContact(name string, email string) Contact {
+	return Contact{
+		Name:  name,
+		Email: email,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			newContact("John", "johndoe@gmail.com"),
+			newContact("Jane", "janedoe@gmail.com"),
+		},
+	}
+}
+
 type Count struct {
 	Count int
 }
@@ -28,16 +55,19 @@ func main() {
 	e := echo.New()
 	e.Use(middleware.Logger())
 
-	count := Count{Count: 0}
+	data := newData()
 	e.Renderer = newTemplate()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", count)
+		return c.Render(200, "index", data)
 	})
 
-	e.POST("/count", func(c echo.Context) error {
-		count.Count++
-		return c.Render(200, "count", count)
+	e.POST("/contacts", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("email")
+
+		data.Contacts = append(data.Contacts, newContact(name, email))
+		return c.Render(200, "index", data)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
